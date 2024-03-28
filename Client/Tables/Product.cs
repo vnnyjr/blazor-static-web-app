0937b5ff450d5fa837192da6eaf69556ddbdf540
VnnyJr <vnny621@gmail.com>
2024-03-27 20:27:20 -0400

    attemtp to add radzen button and cs file

---
+++ Client/Tables/Product.cs
@@ -0,0 +1,9 @@
+namespace RadzenBlazorAzureStaticWebApp.Tables 
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
