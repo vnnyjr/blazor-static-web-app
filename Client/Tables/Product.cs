2f5229ad3e098c2fdee0f845fb48f170fa9cdba9
VnnyJr <vnny621@gmail.com>
2024-03-28 20:27:47 -0400

    error fixes

--- Client/Tables/Product.cs
+++ Client/Tables/Product.cs
@@ -3,7 +3,7 @@ namespace RadzenBlazorAzureStaticWebApp.Tables
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public decimal Price { get; set; }
     }
 }
