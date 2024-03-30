b47fc08b0abc237d6fea0f8f3425a3bd9c1e3a1b
VnnyJr <vnny621@gmail.com>
2024-03-29 20:09:16 -0400

    added videos class, added code to make api call results into videos class objects,

---
+++ Client/Tables/Videos.cs
@@ -0,0 +1,11 @@
+namespace RadzenBlazorAzureStaticWebApp.Tables 
+{
+    public class Videos
+    {
+        public string? Title { get; set; }
+        public string? Channel { get; set; }
+        public DateTime? Published { get; set; }
+        public string? Duration { get; set; }
+        public string? Url { get; set; }
+    }
+}
