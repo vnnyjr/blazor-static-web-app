a67d5cd8f31b5c1a0d388008cc37e33401a97e99
VnnyJr <vnny621@gmail.com>
2024-03-29 20:34:26 -0400

    tweak grid style and fix title error

--- Client/Tables/Videos.cs
+++ Client/Tables/Videos.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Text.Json.Serialization;
+
 namespace RadzenBlazorAzureStaticWebApp.Tables 
 {
     public class Videos
     {
+        [JsonPropertyName("title")]
         public string? Title { get; set; }
         public string? Channel { get; set; }
         public DateTime? Published { get; set; }
