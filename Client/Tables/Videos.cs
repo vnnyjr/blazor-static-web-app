4dda80fb95b5f7a6df5a1b98b58fab54164b44ac
VnnyJr <vnny621@gmail.com>
2024-04-04 19:36:13 -0400

    added new class structure to Videos class and also added new thumbnail column

--- Client/Tables/Videos.cs
+++ Client/Tables/Videos.cs
@@ -3,6 +3,21 @@ using System.Text.Json.Serialization;
 
 namespace RadzenBlazorAzureStaticWebApp.Tables 
 {
+        public class Thumbnail
+    {
+        public ThumbnailUrl? Default { get; set; }
+        public ThumbnailUrl? Medium { get; set; }
+        public ThumbnailUrl? High { get; set; }
+        public ThumbnailUrl? Standard { get; set; }
+        public ThumbnailUrl? Maxres { get; set; }
+    }
+
+    public class ThumbnailUrl
+    {
+        public string? Url { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
     public class Videos
     {
         [JsonPropertyName("title")]
@@ -11,5 +26,6 @@ namespace RadzenBlazorAzureStaticWebApp.Tables
         public DateTime? Published { get; set; }
         public string? Duration { get; set; }
         public string? Url { get; set; }
+        public Thumbnail? Thumbnail { get; set; }
     }
 }
