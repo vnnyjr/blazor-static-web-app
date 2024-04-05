f1bc19317fe7d2da216f4985d48ec10f5f1a0baf
VnnyJr <vnny621@gmail.com>
2024-04-04 20:36:59 -0400

    classes fixes because case sensitiv

--- Client/Tables/Videos.cs
+++ Client/Tables/Videos.cs
@@ -5,17 +5,25 @@ namespace RadzenBlazorAzureStaticWebApp.Tables
 {
         public class Thumbnail
     {
+        [JsonPropertyName("default")]
         public ThumbnailUrl? Default { get; set; }
+        [JsonPropertyName("medium")]
         public ThumbnailUrl? Medium { get; set; }
+        [JsonPropertyName("high")]
         public ThumbnailUrl? High { get; set; }
+        [JsonPropertyName("standard")]
         public ThumbnailUrl? Standard { get; set; }
+        [JsonPropertyName("maxres")]
         public ThumbnailUrl? Maxres { get; set; }
     }
 
     public class ThumbnailUrl
     {
+        [JsonPropertyName("url")]
         public string? Url { get; set; }
+        [JsonPropertyName("width")]
         public int Width { get; set; }
+        [JsonPropertyName("height")]
         public int Height { get; set; }
     }
     public class Videos
@@ -26,6 +34,7 @@ namespace RadzenBlazorAzureStaticWebApp.Tables
         public DateTime? Published { get; set; }
         public string? Duration { get; set; }
         public string? Url { get; set; }
+        [JsonPropertyName("thumbnail")]
         public Thumbnail? Thumbnail { get; set; }
     }
 }
